5ee657d9b78c4b1a9da7c30749f7ae91d8f5afe6
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2023-09-26 20:49:23 -0300

    Cambios en Navbar

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import request
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def registro(request):
+    return render(request,'registro.html')
