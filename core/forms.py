197b885bdad76cd93ed6a9bfdffcc7ea9ebcbe1c
Jordan <jor.poblete@hotmail.com>
2023-10-06 18:13:34 -0300

    funciona el forms wonnn

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Oferta
+from .models import Oferta, TipoCargo
 
 
-
-class OfertaForm(ModelForm):
-
+class OfertaForm(forms.ModelForm):
     class Meta:
         model = Oferta
-        fields = ['id_oferta','nom_oferta','descripcion_oferta','fecha_oferta', 'fk_id_tipo_cargo']
+        fields = ['nom_oferta', 'descripcion_oferta', 'fecha_oferta', 'fk_id_tipo_cargo']
 
+    fk_id_tipo_cargo = forms.ModelChoiceField(
+        queryset=TipoCargo.objects.all(),
+        empty_label=None,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Personaliza las etiquetas del campo fk_id_tipo_cargo
+        self.fields['fk_id_tipo_cargo'].label_from_instance = self.label_from_tipo_cargo_instance
 
+    def label_from_tipo_cargo_instance(self, obj):
+        return obj.nom_cargo
