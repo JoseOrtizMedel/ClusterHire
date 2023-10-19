748f8195d9c6ccb18a0494d9a151acd561fe2b87
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2023-10-19 20:29:15 -0300

    pequeños cambios

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import home, nueva_oferta, ofertas_admin, ofertas_user, register
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import  PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView,PasswordResetCompleteView
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('', home, name="home"),
@@ -16,15 +16,21 @@ urlpatterns = [
 
     path('register', register, name="register"),
 
-    path('password-reset/', 
-        auth_views.PasswordResetView.as_view(
-            template_name='users/password_reset.html',
-            html_email_template_name='users/password_reset_email.html'
-        ),
-        name='password-reset'
-    ),
-    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
-]
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html',
+             html_email_template_name='users/password_reset_email.html'
+         ),
+         name='password-reset'
+         ),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ), name='password_reset_done'),
 
+
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+]
